65bcc5306dab974a245975498a5ea7414b61784e
Johan Gace <gace.johan3@gmail.com>
2020-12-17 12:56:39 -0500

    Revert "Google Places api activation"
    This reverts commit 64cbba93d20452bb3b6a1af11126f4624fe89e6f.

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,7 +1,3 @@
-import '../sass/style.scss'
+import '../sass/style.scss';
 
-import { $, $$ } from './modules/bling'
-import autocomplete from './modules/autocomplete'
-
-//$ instead of document.querySelector
-autocomplete($('#address'), $('#lat'), $('#lng'))
+import { $, $$ } from './modules/bling';
